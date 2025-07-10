5bb8e82b658c6797ee8fdb8a1a47836f26799771
NguyenVietQuan2004 <nguyenvietquan.30032004@gmail.com>
2025-07-10 14:27:57 +0700

    a

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // ✅ Thêm rule override ở đây
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  },
 ];
 
 export default eslintConfig;
